6672b7654363298fa6bfab61563a5c0285527c75
raja9113 <63195895+raja9113@users.noreply.github.com>
2024-06-18 23:03:06 +0530

    Update login.php

--- login.php
+++ login.php
@@ -4,14 +4,14 @@ include('includes/header.php');
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $email = $_POST['email'];
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
     $password = $_POST['password'];
 
     $query = "SELECT * FROM users WHERE email='$email'";
     $result = mysqli_query($conn, $query);
     $user = mysqli_fetch_assoc($result);
 
-    if (password_verify($password, $user['password'])) {
+    if ($user && password_verify($password, $user['password'])) {
         $_SESSION['user_id'] = $user['id'];
         if ($user['is_admin']) {
             header('Location: admin.php');
