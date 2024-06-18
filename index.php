d89fc3d8d757fc1e644a29f5cf0e4f2f6afb37c8
raja9113 <63195895+raja9113@users.noreply.github.com>
2024-06-18 22:33:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-echo "Welcome to the Shopping Complex!";
-?>
+<?php include('includes/header.php'); ?>
+<div class="jumbotron">
+    <h1 class="display-4">Welcome to the Shopping Complex!</h1>
+    <p class="lead">Explore various vendors and their offerings. Book services and time slots easily.</p>
+    <hr class="my-4">
+    <p>Register or login to start booking services.</p>
+    <a class="btn btn-primary btn-lg" href="register.php" role="button">Register</a>
+    <a class="btn btn-secondary btn-lg" href="login.php" role="button">Login</a>
+</div>
+<?php include('includes/footer.php'); ?>
