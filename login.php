6cbcc82f68a2787b7575fa97a013f45235322b4b
raja9113 <63195895+raja9113@users.noreply.github.com>
2024-06-18 23:01:18 +0530

    Update login.php

--- login.php
+++ login.php
@@ -13,7 +13,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if (password_verify($password, $user['password'])) {
         $_SESSION['user_id'] = $user['id'];
-        header('Location: index.php');
+        if ($user['is_admin']) {
+            header('Location: admin.php');
+        } else {
+            header('Location: index.php');
+        }
     } else {
         echo "<div class='alert alert-danger' role='alert'>Invalid credentials!</div>";
     }
